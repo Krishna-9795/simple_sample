2ab042d7c34bc663f85915802cac70f8baf3aa3c
Krishna-9795 <krishna@cartoonmangos.com>
2023-07-24 13:06:15 +0530

    4.1

---
+++ routes.py
@@ -0,0 +1,10 @@
+/register, method=['POST']
+/login,methods=['POST']
+/profile, methods=['GET']
+/users, methods=['GET']
+/users/create, methods=['POST']
+/uploads, methods=['POST']
+/users/update/<int:user_id>, methods=['PUT']
+/users/delete/<int:user_id>, methods=['DELETE']
+
+
